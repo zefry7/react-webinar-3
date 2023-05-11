e1dff4ee7106bd90a8a518f02a8a43d04d024329
Vladimir Shestakov <boolive@yandex.ru>
2023-05-12 01:35:26 +0300

    useState

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import './style.css';
 import {plural} from "../../utils";
@@ -7,12 +7,21 @@ function Item({item, onDelete, onSelect}){
 
   console.log('Item', item.code);
 
+  const [count, setCount] = useState(0);
+
+  const onClick = () => {
+    onSelect(item.code);
+    if (!item.selected) {
+      setCount(count + 1);
+    }
+  }
+
   return (
     <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-         onClick={() => onSelect(item.code)}>
+         onClick={onClick}>
       <div className='Item-code'>{item.code}</div>
       <div className='Item-title'>
-        {item.title} {item.count && ` | Выделяли ${item.count} ${plural(item.count, {one: 'раз', few: 'раза', many: 'раз'})}`}
+        {item.title} {count ? ` | Выделяли ${count} ${plural(count, {one: 'раз', few: 'раза', many: 'раз'})}` : ''}
       </div>
       <div className='Item-actions'>
         <button onClick={() => onDelete(item.code)}>
