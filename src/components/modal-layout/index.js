9ac40fd3c94f6e434d6b48a65a2364258d1902b3
Vladimir Shestakov <boolive@yandex.ru>
2023-05-23 22:53:54 +0300

    modal-center

--- src/components/modal-layout/index.js
+++ src/components/modal-layout/index.js
@@ -1,4 +1,4 @@
-import {memo} from "react";
+import {memo, useEffect, useRef} from "react";
 import PropTypes from "prop-types";
 import {cn as bem} from '@bem-react/classname';
 import './style.css';
@@ -7,9 +7,29 @@ function ModalLayout(props) {
 
   const cn = bem('ModalLayout');
 
+  // Корректировка центра, если модалка больше окна браузера.
+  const layout = useRef();
+  const frame = useRef();
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(() => {
+      // Центрирование frame или его прижатие к краю, если размеры больше чем у layout
+      layout.current.style.alignItems = (layout.current.clientHeight < frame.current.clientHeight)
+        ? 'flex-start'
+        : 'center';
+      layout.current.style.justifyContent = (layout.current.clientWidth < frame.current.clientWidth)
+        ? 'flex-start'
+        : 'center';
+    });
+    // Следим за изменениями размеров layout
+    resizeObserver.observe(layout.current);
+    return () => {
+      resizeObserver.disconnect();
+    }
+  });
+
   return (
-    <div className={cn()}>
-      <div className={cn('frame')}>
+    <div className={cn()} ref={layout}>
+      <div className={cn('frame')} ref={frame}>
         <div className={cn('head')}>
           <h1 className={cn('title')}>{props.title}</h1>
           <button className={cn('close')} onClick={props.onClose}>Закрыть</button>
@@ -30,7 +50,8 @@ ModalLayout.propTypes = {
 
 ModalLayout.defaultProps = {
   title: 'Модалка',
-  onClose: () => {}
+  onClose: () => {
+  }
 };
 
 export default memo(ModalLayout);
