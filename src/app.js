2081cab226381c453c805902415aadfa90a63a0b
Vladimir Shestakov <boolive@yandex.ru>
2024-02-21 09:36:12 +0300

    fix ci

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import './styles.css';
 
 /**
  * Приложение
- * @param store {Store} Хранилище состояния приложения
+ * @param store {Store} Состояние приложения
  * @returns {React.ReactElement}
  */
 function App({store}) {
