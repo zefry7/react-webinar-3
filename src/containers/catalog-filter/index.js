b49955f70b825c0cb95b47735f552dd35145f92d
nikita petrov <petrov.nikita702@mail.ru>
2024-10-07 15:49:26 +0300

    исправления 07_10

--- src/containers/catalog-filter/index.js
+++ src/containers/catalog-filter/index.js
@@ -44,48 +44,7 @@ function CatalogFilter() {
   };
 
   useEffect(() => {
-
-    const fetchCategory = async () => {
-      const respone = await fetch("/api/v1/categories?fields=_id,title,parent(_id)&limit=*")
-      const json = await respone.json()
-
-      let res = []
-      let arr = []
-      let map = new Map()
-
-      for (const x of json.result.items) {
-        map.set(x._id, { title: x.title, parent: x.parent?._id || null })
-      }
-
-      for (const x of json.result.items) {
-        if (map.get(x._id).parent != null) {
-          arr.push([{ title: map.get(map.get(x._id).parent).title, id: map.get(x._id).parent }, { title: x.title, id: x._id }])
-        } else {
-          arr.push([null, { title: x.title, id: x._id }])
-        }
-      }
-
-      function ttt(value, line) {
-        for (let i = 0; i < arr.length; ++i) {
-          if (arr[i][0]?.title == value.title) {
-            ttt(arr[i][1], line + "- ")
-          }
-        }
-        res.push({ title: line + "" + value.title, value: value.id })
-      }
-
-      for (let i = 0; i < arr.length; ++i) {
-        if (arr[i][0] == null) {
-          ttt(arr[i][1], "")
-        }
-      }
-
-      res.reverse()
-
-      store.actions.category.setCategoryList(res)
-    }
-
-    fetchCategory()
+    store.actions.category.fetchCategory()
   }, [])
 
   return (
--- src/store/category/index.js
+++ src/store/category/index.js
@@ -11,8 +11,42 @@ class CategoryState extends StoreModule {
         };
     }
 
-    setCategoryList(list) {
-        this.setState({ ...this.getState(), categoryList: [{ value: "all", title: "Все" }, ...list] })
+    async fetchCategory() {
+        const respone = await fetch("/api/v1/categories?fields=_id,title,parent(_id)&limit=*")
+        const json = await respone.json()
+
+        let res = []
+        let arr = []
+        let map = new Map()
+
+        for (const x of json.result.items) {
+            map.set(x._id, { title: x.title, parent: x.parent?._id || null })
+        }
+
+        for (const x of json.result.items) {
+            if (map.get(x._id).parent != null) {
+                arr.push([{ title: map.get(map.get(x._id).parent).title, id: map.get(x._id).parent }, { title: x.title, id: x._id }])
+            } else {
+                arr.push([null, { title: x.title, id: x._id }])
+            }
+        }
+
+        function ttt(value, line) {
+            res.push({ title: line + "" + value.title, value: value.id })
+            for (let i = 0; i < arr.length; ++i) {
+                if (arr[i][0]?.title == value.title) {
+                    ttt(arr[i][1], line + "- ")
+                }
+            }
+        }
+
+        for (let i = 0; i < arr.length; ++i) {
+            if (arr[i][0] == null) {
+                ttt(arr[i][1], "")
+            }
+        }
+
+        this.setState({ ...this.getState(), categoryList: [{ value: "all", title: "Все" }, ...res] })
     }
 
 }
